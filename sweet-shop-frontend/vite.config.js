45263a44cbe1a8fdfef4c8ae976d92e6c4e8032b
Om Gupta <omgupta2473@gmail.com>
2025-12-14 18:46:48 +0530

    Allowed access to replit

--- sweet-shop-frontend/vite.config.js
+++ sweet-shop-frontend/vite.config.js
@@ -1,10 +1,11 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5173,
-  },
-});
+    allowedHosts: [
+      'a3328c77-30ec-47da-b7f5-4272b6de7997-00-3ig221athbn06.pike.replit.dev'
+    ]
+  }
+})
